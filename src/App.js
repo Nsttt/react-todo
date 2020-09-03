c7e15060f202979074576600df4c826e2b095157
Nestor <elnestinatorHD@gmail.com>
2020-09-03 18:12:09 +0200

    First design.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputTex={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
